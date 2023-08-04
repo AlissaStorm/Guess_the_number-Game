4232f816bde1178beead3d8a0c8bb7f4dc0595d6
AlissaStorm <Alissaalidib@hotmail.com>
2023-08-04 02:00:24 +0200

    Spil

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq.Expressions;
+
+namespace gæt_nummeret_spil
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random= new Random();
+
+            bool state = true;
+            while (state == true)
+            {
+                int nummer = random.Next(1, 11);
+                
+
+                bool gæt = true;
+                while (gæt == true)
+                {
+                    Console.WriteLine("Vælg et tal fra 1 til 10");
+                    int værdi = Convert.ToInt32(Console.ReadLine());
+
+                    if (nummer == værdi)
+                    {
+                        Console.WriteLine("Flot klaret. Det er rigtigt!");
+                        gæt = false;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Dumme idiot, Forkert. Prøv igen.");
+                    }
+                   
+                }
+                Console.WriteLine("Vil du spille igen?");
+                
+                string igen = Console.ReadLine();
+                if(igen == "nej" || igen == "Nej")
+                {
+                    state = false; 
+                }
+                else
+                {
+                    state = true;
+                }
+
+            }
+
+            
+            
+
+
+
+        }
+    }
+}
+
+
